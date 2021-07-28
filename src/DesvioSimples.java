e805071399f0b9495813286e4caaa6149167ecf0
Honorato <jaimehonoratos@gmail.com>
2021-07-28 20:45:30 -0300

    Implementado o IF

--- src/DesvioSimples.java
+++ src/DesvioSimples.java
@@ -14,6 +14,12 @@ public class DesvioSimples {
 		System.out.println("Deixe-me verificar sua idade...");
 		/*Aqui devemos criar uma solução para verificar a idade do usuário*/
 		
+		if (idade < 12) {
+			System.out.println("Você não Pode Jogar");
+		}else {
+			System.out.println("Você PODE Jogar !!");
+		}
+		
 		leitor.close();
 
 	}
